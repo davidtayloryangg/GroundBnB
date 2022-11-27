554ac4f291fd69e1fcfb19b962958757affb9f65
David Yang <davidtayloryang@gmail.com>
2022-11-27 16:07:53 -0500

    ts init for backend

---
+++ server/src/app.ts
@@ -0,0 +1,32 @@
+import * as express from 'express';
+import { Request, Response } from 'express';
+import Routes from './routes/index.ts';
+
+let totalRequest: number = 0;
+let dict: object = {};
+
+class App {
+    public app: express.Application;
+    public routePrv: Routes = new Routes();
+
+    constructor() {
+        this.app = express();
+        this.config();
+        this.routePrv.routes(this.app);
+    }
+
+    Logger = (req: Request, res: Response, next: Function) => {
+        totalRequest++;
+        dict[totalRequest] = req.url;
+        console.log(`Request #${totalRequest}: ${req.url}`);
+        next();
+    }
+
+    private config(): void {
+        this.app.use(express.json());
+        this.app.use(express.urlencoded({ extended: false }));
+        this.app.use(this.Logger);
+    }
+}
+
+export default new App().app;
