ea9b7873542cc592db852ec2ffd8e1839487bcc2
David <davidtayloryang@gmail.com>
2022-11-28 15:09:44 -0500

    comments

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,6 +1,8 @@
 import {userRoutes} from "./users"
+import {listingRoutes} from "./listings"
 import * as express from "express";
 
 export const routes = express.Router();
 
 routes.use("/users", userRoutes);
+routes.use("/listings", listingRoutes);
