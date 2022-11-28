fe9f50a9393ca51ba61d3b55b82f7e210f2f4f0f
David <davidtayloryang@gmail.com>
2022-11-28 11:15:01 -0500

    folder set up

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,18 +1,18 @@
 import * as express from 'express';
 import { Request, Response } from 'express';
-import Routes from './routes/index.ts';
+//import Routes from './routes/index.ts';
 
 let totalRequest: number = 0;
 let dict: object = {};
 
 class App {
     public app: express.Application;
-    public routePrv: Routes = new Routes();
+    //public routePrv: Routes = new Routes();
 
     constructor() {
         this.app = express();
         this.config();
-        this.routePrv.routes(this.app);
+        //this.routePrv.routes(this.app);
     }
 
     Logger = (req: Request, res: Response, next: Function) => {
