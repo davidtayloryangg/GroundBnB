9db1b0444d3ae46564e032491f14534216e3eb56
David <davidtayloryang@gmail.com>
2022-11-28 23:50:50 -0500

    change in listing but dont see anything -- commiting anyway just incase

--- server/src/routes/listings.ts
+++ server/src/routes/listings.ts
@@ -6,7 +6,7 @@ import * as distance from "geo-distance"
 
 
 /**Getting listing and sorting from closest to furthest 
- * Send request to /search/location?lat=xxxx&lon=xxx
+ * Client send request to /search/location?lat=xxxx&lon=xxx
 */
 listingRoutes.get('/search/location', async (req: Request, res: Response) => {
     console.log("GET /listings/search/location");
